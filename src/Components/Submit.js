0241481de6606cdf00b64d5c42788a50cc8cd2fb
caballo <imanol.l.p@hotmail.com>
2022-04-13 21:57:24 +0200

    Fixed object not being saved if no supplements

--- src/Components/Submit.js
+++ src/Components/Submit.js
@@ -1,65 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import LoadingButton from '@mui/lab/LoadingButton';
 import SaveIcon from '@mui/icons-material/Save';
 
-import {
-	Box
-} from '@mui/material';
-
+import { Box } from '@mui/material';
 
 export default function Submit(props) {
-    const {newMenuItem, stats } = props
+	const { newMenuItem, stats } = props;
 
-    const [sending, setSending] = useState(false);
+	const [sending, setSending] = useState(false);
 
-
-    function mockDBRes() {
-
-        console.log(sanitizeData())
+	function mockDBRes() {
+		console.log(sanitizeData());
 		setSending(true);
 		setTimeout(() => setSending(false), 4000);
 	}
 
-    function sanitizeData() {
-        const rawData = { ...newMenuItem, ...stats()}
-		
-		const ceil2Int = (num) => Math.ceil(num * 100) / 100 
-		
-		
-		rawData.finalWeight = Number(rawData.finalWeight)
-		rawData.prepTime = Number(rawData.prepTime)
-		rawData.rationNumber = Number(rawData.rationNumber)
-		
-		if(!rawData.timeFormat) rawData.timeFormat = 'm'
+	function sanitizeData() {
+		const rawData = { ...newMenuItem, ...stats() };
 
-		rawData.profitPerRation = ceil2Int(rawData.profitPerRation)
-		
-        
-		rawData.pvp = ceil2Int(rawData.pvp)
-		rawData.timestamp = new Date()
-		rawData.ingredients = rawData.items.map(item => item._id)
-		rawData.quantities = rawData.items.map(item => {
+		const ceil2Int = (num) => Math.ceil(num * 100) / 100;
+
+		rawData.finalWeight = Number(rawData.finalWeight);
+		rawData.prepTime = Number(rawData.prepTime);
+		rawData.rationNumber = Number(rawData.rationNumber);
+
+		if (!rawData.timeFormat) rawData.timeFormat = 'm';
+
+		rawData.profitPerRation = ceil2Int(rawData.profitPerRation);
+
+		rawData.pvp = ceil2Int(rawData.pvp);
+		rawData.timestamp = new Date();
+		rawData.ingredients = rawData.items.map((item) => item._id);
+		rawData.quantities = rawData.items.map((item) => {
 			return {
 				ingredient: item._id,
-				quantity: Number(item.quantity)
-			}
-		})
-		rawData.supplements = rawData.supplements.map(item => {
-			delete item.id
-			
-			return {
-				concept: item.concept,
-				percentage: Number(item.percentage)
-			}
-		})
+				quantity: Number(item.quantity),
+			};
+		});
 
-		delete rawData.items
+		if (rawData.supplements) {
+			rawData.supplements = rawData.supplements.map((item) => {
+				delete item.id;
 
-		return rawData
-    }
+				return {
+					concept: item.concept,
+					percentage: Number(item.percentage),
+				};
+			});
+		}
 
-  return (
-    <div>
+		delete rawData.items;
+
+		return rawData;
+	}
+
+	return (
+		<div>
 			<Box sx={{ display: 'flex', justifyContent: 'center', mb: 7 }}>
 				<LoadingButton
 					loading={sending}
@@ -71,7 +67,6 @@ export default function Submit(props) {
 					Guardar
 				</LoadingButton>
 			</Box>
-
-    </div>
-  )
+		</div>
+	);
 }
